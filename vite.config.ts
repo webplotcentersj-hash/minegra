e3cbbff047a59e21804f9778810b28c5bb18973e
webplotcentersj-hash <web.plotcentersj@gmail.com>
2026-05-06 08:38:36 -0300

    feat: configuracion de PWA instalable (network only)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(), 
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: [],
+        runtimeCaching: [],
+      },
+      manifest: {
+        name: 'Feria Minera',
+        short_name: 'FeriaMinera',
+        description: 'Aplicación de Trivia para Feria Minera',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
